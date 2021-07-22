449193b29355cd04e22e5f38b2fc84e95dfd1640
vetcat <vetcat@yandex.ru>
2021-07-22 15:21:23 +0300

    UiHintsView layout

---
+++ Assets/Scripts/SampleScene/UiViews/Views/UiHints/UiHintSimpleView.cs
@@ -0,0 +1,14 @@
+using Libs.OpenUI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SampleScene.UiViews.Views.UiHints
+{
+    public class UiHintSimpleView : UiView
+    {
+        public RectTransform Body;
+        public CanvasGroup CanvasGroup;
+        public Text TextDescription;
+        public RectTransform Pointer;
+    }
+}
---
+++ Assets/Scripts/SampleScene/UiViews/Views/UiHints/UiHintsView.cs
@@ -0,0 +1,11 @@
+using Libs.OpenUI;
+using UnityEngine.UI;
+
+namespace SampleScene.UiViews.Views.UiHints
+{
+    public class UiHintsView : UiView
+    {
+        public Image ImageHintBackground;
+        public UiHintSimpleView HintSimpleView;
+    }
+}
