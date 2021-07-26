04e9d7e3378d3f83753a4e7eed9b45cf375d19e5
vetcat <vetcat@yandex.ru>
2021-07-26 17:59:30 +0300

    UiIndicatorItemView improvement - add localization

--- Assets/Scripts/SampleScene/UiViews/Views/UiObjectIndicator/UiIndicatorItemView.cs
+++ Assets/Scripts/SampleScene/UiViews/Views/UiObjectIndicator/UiIndicatorItemView.cs
@@ -1,14 +1,17 @@
 using Libs.OpenUI;
+using Libs.OpenUI.Localization;
 using UnityEngine.UI;
 
 namespace SampleScene.UiViews.Views.UiObjectIndicator
 {
-    public class UiIndicatorItemView : UiView
+    public class UiIndicatorItemView : UiView, ILocalizable
     {
         public Image ImageIcon;
         public Button ButtonAction;
         public Text TextInfo;
         public Slider Slider;
         public Text TextSliderValue;
+        [Localization("ClickToReward")]
+        public Text TextClickToReward;
     }
 }
